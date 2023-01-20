36b3128750435a4f8f83d77ce86b8c520fe9743c
Alessio Parma <alessio.parma@gmail.com>
2023-01-20 19:09:13 +0100

    ensure sub statuses match docs
    https://www.zerobounce.net/docs/email-list-validation/status-codes

--- zerobouncesdk/zb_validate_response.py
+++ zerobouncesdk/zb_validate_response.py
@@ -15,9 +15,9 @@ class ZBValidateResponse(ZBResponse):
     sub_status: ZBValidateSubStatus = None
     """one of [antispam_system, greylisted, mail_server_temporary_error, forcible_disconnect, 
     mail_server_did_not_respond, timeout_exceeded, failed_smtp_connection, mailbox_quota_exceeded, 
-    exception_occurred, possible_traps, role_based, global_suppression, mailbox_not_found, no_dns_entries, 
+    exception_occurred, possible_trap, role_based, global_suppression, mailbox_not_found, no_dns_entries, 
     failed_syntax_check, possible_typo, unroutable_ip_address, leading_period_removed, does_not_accept_mail, 
-    alias_address, role_based_catch_all, disposable, toxic] """
+    alias_address, role_based_catch_all, disposable, toxic, alternate, mx_forward] """
 
     account: str = None
     """The portion of the email address before the "@" symbol."""
