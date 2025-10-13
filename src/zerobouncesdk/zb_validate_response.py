8356ccb3055baf17ad7afe7b404a8de947105ff2
Drazen Pavetic <drazenzen@gmail.com>
2025-10-13 11:42:30 +0200

    Add role_based_accept_all sub status

--- src/zerobouncesdk/zb_validate_response.py
+++ src/zerobouncesdk/zb_validate_response.py
@@ -18,7 +18,8 @@ class ZBValidateResponse(ZBResponse):
     mail_server_did_not_respond, timeout_exceeded, failed_smtp_connection, mailbox_quota_exceeded,
     exception_occurred, possible_trap, role_based, global_suppression, mailbox_not_found, no_dns_entries,
     failed_syntax_check, possible_typo, unroutable_ip_address, leading_period_removed, does_not_accept_mail,
-    alias_address, role_based_catch_all, disposable, toxic, alternate, mx_forward, blocked, allowed, accept_all]"""
+    alias_address, role_based_catch_all, disposable, toxic, alternate, mx_forward, blocked, allowed, accept_all,
+    role_based_accept_all]"""
 
     account: str = None
     """The portion of the email address before the "@" symbol."""
